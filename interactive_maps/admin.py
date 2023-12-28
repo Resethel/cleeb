b2b8daa266c966d1bbb1940971fb0cac21c25d67
Raphaël Ollando <raphael.ollando@uni.lu>
2023-12-28 01:23:53 +0100

    feat(interactive_maps): introduce initial admin configuration
    - Add admin classes for `Author`, `Map`, `Section`, and `Text` models
    - Implement inline admin classes for `Author`, `Text`, and `Section` models
    - Register models with their respective admin classes

--- interactive_maps/admin.py
+++ interactive_maps/admin.py
@@ -1,3 +1,76 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Map, Section, Text
+
+
+# ======================================================================================================================
+# Admin classes for the Section model
+# ======================================================================================================================
+
+class SectionAdmin(admin.ModelAdmin):
+    """Admin class for the ThematicMapTextSection model."""
+# End class SectionAdmin
+
+admin.site.register(Section, SectionAdmin)
+
+# ======================================================================================================================
+# Admin classes for the Text
+# ======================================================================================================================
+
+class SectionInline(admin.StackedInline):
+    """Inline class for the ThematicMapTextSection model."""
+    model = Section
+    extra = 1
+    show_change_link = True
+# End class SectionInline
+
+class TextAdmin(admin.ModelAdmin):
+    """Admin class for the Text model."""
+
+    inlines = [
+        SectionInline, # Inline for the ThematicMapTextSection model
+    ]
+# End class TextAdmin
+
+admin.site.register(Text, TextAdmin)
+
+# ======================================================================================================================
+# Admin classes for the Author model
+# ======================================================================================================================
+
+class AuthorAdmin(admin.ModelAdmin):
+    """Admin class for the Author model."""
+# End class AuthorAdmin
+
+admin.site.register(Author, AuthorAdmin)
+
+
+# ======================================================================================================================
+# Admin classes for the Map model
+# ======================================================================================================================
+
+class TextInline(admin.StackedInline):
+    """Inline class for the Text model."""
+    model = Text
+    extra = 1
+    show_change_link = True
+# End class TextInline
+
+class AuthorInline(admin.TabularInline):
+    """Inline class for the Author model."""
+    model = Map.authors.through
+    extra = 1
+
+    show_change_link = True
+# End class AuthorInline
+
+class MapAdmin(admin.ModelAdmin):
+    """Admin class for ThematicMap model."""
+
+    inlines = [
+        AuthorInline, # Inline for the Author model
+        TextInline, # Inline for the Text model
+    ]
+# End class MapAdmin
+
+admin.site.register(Map, MapAdmin)
