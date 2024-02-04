c56d0f3b7aef022d0dc806415d219d5088ac7c23
Raphaël Ollando <raphael.ollando@uni.lu>
2024-02-04 20:22:53 +0100

    feat: add detail view for each datasets

--- datasets/urls.py
+++ datasets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-
+from datasets.views import DatasetDetailView
 
 urlpatterns = [
+    path('jeux-de-donnees/fiche/<slug:slug>/', DatasetDetailView.as_view(), name='dataset-detail'),
 ]
--- datasets/views.py
+++ datasets/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from datasets.models import Dataset
+
+
+# ======================================================================================================================
+# Dataset Detail View
+# ======================================================================================================================
+
+class DatasetDetailView(DetailView):
+    model = Dataset
+    template_name = 'datasets/datasets_detail.html'
+    context_object_name = 'dataset'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['dataset_versions'] = []
+        for version in context['object'].versions.all():
+            v = {'id': version.id, 'date': version.date.strftime("%d %B %Y"), 'file': version.file}
+            context['dataset_versions'].append(v)
+        #
+        # for version in context['dataset'].versions.all():
+        #     version.date = version.date.strftime("%d %B %Y")
+        #
+        # print(context)
+        return context
+# End class DatasetDetailView
