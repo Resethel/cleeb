b0204a856635b5a21c50a663d4660c7faeb254f0
Raphaël Ollando <raphael.ollando@uni.lu>
2024-02-07 17:23:40 +0100

    feat: add datasets search page

--- datasets/urls.py
+++ datasets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from datasets.views import DatasetDetailView
+from datasets.views import DatasetDetailView, DatasetsSearchView
 
 urlpatterns = [
+    path('jeux-de-donnees/', DatasetsSearchView.as_view(), name='datasets-search'),
     path('jeux-de-donnees/fiche/<slug:slug>/', DatasetDetailView.as_view(), name='dataset-detail'),
 ]
--- datasets/views.py
+++ datasets/views.py
@@ -1,8 +1,57 @@
+from django.db.models import Q
+from django.http import HttpRequest
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
-from datasets.models import Dataset
+from datasets.models import Dataset, DatasetCategory
 
+
+# ======================================================================================================================
+# Dataset Main view (search)
+# ======================================================================================================================
+
+class DatasetsSearchView(ListView):
+
+    model = Dataset
+    template_name = 'datasets/datasets_search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = DatasetCategory.objects.all()
+
+        # Add to the context which category is selected
+        category = self.request.GET.get('category')
+        context['selected_category'] = DatasetCategory.objects.get(slug=category) if category else None
+
+        # Add to the context the search query
+        search = self.request.GET.get('search')
+        context['search'] = search if search else None
+
+
+        return context
+
+    def get_queryset(self):
+        datasets = Dataset.objects.filter(public=True)
+
+        # Filter by category
+        category = self.request.GET.get('category')
+        if category:
+            categories = DatasetCategory.objects.filter(slug=category)
+
+
+            datasets = datasets.filter(categories__in=categories)
+
+        # Filter by search query
+        search = self.request.GET.get('search')
+        if search:
+            # TODO: add search by category
+            datasets = datasets.filter(
+                Q(name__icontains=search) |
+                Q(description__icontains=search)
+            )
+
+        return datasets
+# End def datasets_filter_view
 
 # ======================================================================================================================
 # Dataset Detail View
