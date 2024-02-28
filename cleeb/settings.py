8fa4f2368b465fc34f171350279097e8e32a0c05
Raphaël Ollando <raphael.ollando@uni.lu>
2024-02-28 08:37:47 +0100

    chore: enable geospatial database
    - Enable the `django.contrib.gis`
    - Change settings.py to use a postgres+postgis database (align with production settings)
    - Update dependencies

--- cleeb/settings.py
+++ cleeb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 
     # Third-party apps
     'colorfield',
@@ -89,13 +90,17 @@ WSGI_APPLICATION = 'cleeb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'cleeb-dev-db',
+        'USER': 'cleeb',
+        'PASSWORD': 'cleeb',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
