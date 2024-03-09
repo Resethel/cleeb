54948c5c2cde6ca10721f431ceabb39d3bb16bb4
Raphaël Ollando <raphael.ollando@uni.lu>
2024-03-09 23:45:01 +0100

    fix: solve wrong attribute issues in core/views

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Views for the `core` application.
+"""
 from django.shortcuts import render
 from django.views.generic import DetailView
 
 from core.models import Organization
-from map_thematics.models import Thematic
 
 
 # ======================================================================================================================
@@ -30,14 +33,14 @@ class OrganizationDetailView(DetailView):
         context['categories']  = [org_obj.type]
         context['body']        = org_obj.description
         context['social_links'] = []
-        if org_obj.contact:
-            context['social_links'].append({'name': 'email', 'url': org_obj.contact})
+        if org_obj.email:
+            context['social_links'].append({'name': 'email', 'url': org_obj.email})
         if org_obj.website:
             context['social_links'].append({'name': 'website', 'url': org_obj.website})
         if org_obj.facebook:
             context['social_links'].append({'name': 'facebook', 'url': org_obj.facebook})
-        if org_obj.twitter:
-            context['social_links'].append({'name': 'twitter', 'url': org_obj.twitter})
+        if org_obj.twitter_x:
+            context['social_links'].append({'name': 'twitter', 'url': org_obj.twitter_x})
         if org_obj.instagram:
             context['social_links'].append({'name': 'instagram', 'url': org_obj.instagram})
 
