ccc53cca010fb500de4081439649db85ecc3f262
Raphaël Ollando <raphael.ollando@uni.lu>
2024-03-18 15:29:17 +0100

    refactor(datasets): update datasets-index template and view to be consistent with the style introduced in the previous commit

--- static/js/datasets_search.js
+++ static/js/datasets_search.js
@@ -1,24 +1,25 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function datasets_search() {
-  var elements = document.getElementsByClassName("search-result__item__categories__more");
-  for(var i = 0; i < elements.length; i++)
-  {
-    elements[i].classList.toggle("--show");
-  }
+function datasets_search() {var elements = document.getElementsByClassName("dataset-categories__more");
+    for(var i = 0; i < elements.length; i++)
+    {
+        elements[i].classList.toggle("--show");
+    }
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.closest('.search-result__item__categories__more__button'))
-  {
-    var dropdowns = document.getElementsByClassName("search-result__item__categories__more");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('--show')) {
-        openDropdown.classList.remove('--show');
-      }
+    if (!event.target.closest('.dataset-categories__more__button'))
+    {
+        var dropdowns = document.getElementsByClassName("dataset-categories__more");
+        var i;
+        for (i = 0; i < dropdowns.length; i++)
+        {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('--show'))
+            {
+                openDropdown.classList.remove('--show');
+            }
+        }
     }
-  }
 }
