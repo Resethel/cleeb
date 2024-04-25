c6b846a358be9688aee87ee6c69fb7aba60f6c23
Raphaël Ollando <raphael.ollando@uni.lu>
2024-04-25 16:10:04 +0200

    feat(localization): add locale strings to the `contact` application

--- contact/apps.py
+++ contact/apps.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Apps configuration for the `contact` application.
+"""
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class ContactConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact'
+    verbose_name = _('Contact form submissions')
+# End class ContactConfig
