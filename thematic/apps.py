9c5181bce99eb003870540267ac101b3e1253ec6
Raphaël Ollando <raphael.ollando@uni.lu>
2024-04-26 11:38:42 +0200

    feat(localization): add locale strings to the `thematic` application

--- thematic/apps.py
+++ thematic/apps.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Apps configuration for the `thematic` application.
+"""
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ThematicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'thematic'
+    verbose_name = _("Thematic")
+# End class ThematicConfig
